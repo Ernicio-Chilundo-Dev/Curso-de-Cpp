bd611b1b19416ded0a16933d2171049f38089050
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-01-28 14:49:56 +0200

    aula 6 feita com sucesso, aprende a criar variaveis locais e globais

--- aula06/aula06.cpp
+++ aula06/aula06.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
 using namespace std;
-int main(){
+int n1, n2; // variaveis globais
+int main()
+{
+    //Operadores Matematicos: + - / % * ()
+    int n3, n4; // variaveis locais
+    // int res;
+    int res1, res2;
 
+    n1 =11;
+    n2 = 3;
+    n3 = 5;
+    n4 = 15;
+
+    // res = n1+n2+n3+n4;
+    // res = (n1+n2+n3+n4) - 10;
+    res1 = n1 / n2; 
+    res2 = n1 % n2; // Usado o operador mode "%" para obter o resto da divisao 
+    
+    cout << "Divis: " << res1  << "\n";
+    cout << "Resto: " << res2  << "\n";
     return 0;
 }
