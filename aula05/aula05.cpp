bc465cf1fc8524fe63567ff6ea30a78d46168e91
Ernicio-Chilundo-Dev <erniciojermiaschilund@gmail.com>
2025-01-28 10:11:28 +0200

    aula05 feita com sucesso

---
+++ aula05/aula05.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+/*Multiplas variaveis e constantes #define*/
+#define pi 3.1415
+#define nome cout << "Ernicio Jermias Chilundo \n";
+#define idade cout << "23\n";
+#define genero cout << "M\n";
+
+int main()
+{
+    int vidas = 3, tiros = 500, life = 100;
+
+    cout << pi << "\n\n";
+    cout << "Nome: ";
+    nome
+            cout
+        << "Idade: ";
+    idade
+            cout
+        << "Genero: ";
+    genero return 0;
+}
