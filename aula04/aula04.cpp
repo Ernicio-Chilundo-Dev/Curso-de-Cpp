54e247b4631a36384c3579fa3e05a8ce2ccf383e
Ernicio-Chilundo-Dev <erniciojermiaschilund@gmail.com>
2025-01-28 09:27:04 +0200

    aula4 concluida co sucesso

--- aula04/aula04.cpp
+++ aula04/aula04.cpp
@@ -1,6 +1,39 @@
-#include <iostream>;
+#include <iostream>
+
 using namespace std;
 
-int main(){
+int main()
+{
+    /*Aula 4 ensina como criar variaveis e trabalhar com tipos de variaveis*/
+
+    // TIPO NOME;
+    // TIPO NOME = VALOR;
+    int vidas = 0;        // 10, 20
+    char letra = 'B';     // 'B'
+    double decimal = 2.5; // 2.499999;
+    // float decimal2; //2.5;
+    bool vivo = true;        // true=verdadeiro; false=falso
+    string nome = "Ernicio"; // Ernicio
+
+    cout << "Digite o numero de vidas: ";
+    cin >> vidas;
+
+    cout << "Digite uma letra: ";
+    cin >> letra;
+
+    cout << "Dinheiro: ";
+    cin >> decimal;
+
+    cout << "Digite o seu nome: ";
+    cin >> nome;
+    cout << "\n";
+    cout << "\n";
+
+    cout << vidas << "\n";
+    cout << letra << "\n";
+    cout << decimal << "\n";
+    cout << vivo << "\n";
+    cout << nome << "\n";
+
     return 0;
 }
