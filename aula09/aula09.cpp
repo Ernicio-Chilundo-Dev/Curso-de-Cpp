b34c80255d4113f3d90151254fcfb9717b2847e6
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-01-28 17:57:03 +0200

    trabalhado com comando de decisao IF  ELSE

---
+++ aula09/aula09.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1 = 10;
+    int num2 = 50;
+    int sol = 1;
+    char opc = 's';
+
+    if(sol){
+        cout << "Vou ao clube\n";
+    }else{
+        cout << "Vou ao cinema\n";
+    }
+
+    return 0;
+}
