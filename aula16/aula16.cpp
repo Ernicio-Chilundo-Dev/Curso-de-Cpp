4232a594e72c00d1331607726c8d90f673737f87
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-01-30 10:08:25 +0200

    aula16 feita com sucesso, aprende a trabalhar com loop do while

---
+++ aula16/aula16.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int cont;
+
+    cont =20;
+    do{
+        cout <<"Ernicio = " << cont <<"\n";
+        
+    } while(cont++ < 20);
+
+    cout << "\nRotina finalizada\n\n";
+
+    return 0;
+}
