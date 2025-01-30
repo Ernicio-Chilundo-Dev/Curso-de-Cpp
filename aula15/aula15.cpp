78b4c5b83dd4b7b3e47e493f2965f34c2d22e49b
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-01-30 09:51:35 +0200

    aula15 feita com sucesso aprende a trabalahar com o comando break

---
+++ aula15/aula15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int cont;
+    cont = 0;
+    while (cont < 1000)
+    {
+        system("cls");
+        cout << cont << "\n";
+        if (cont == 500)
+            break;
+        cont++;
+    }
+    return 0;
+}
