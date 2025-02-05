0e129cb98944b888735f2c10fd673066df543c28
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-05 15:53:13 +0200

    aula 21 feita com sucesso aprende a trabalhar com argumentos e a chamar paramentros usando ponteiro

--- aula21/aula21.cpp
+++ aula21/aula21.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
 using namespace std;
-int main(int argc, char * argv[]){
-    cout << argv[1] << "\n";
-    cout << argc << "\n";
+int main(int argc, char *argv[])
+{
+    // cout << argv[3] << "\n";
+    // cout << argc << "\n";
+    if(argc > 1){
+        if(!strcmp(argv[1], "sol")){
+            cout << "Vou ao clube.\n\n";
+        }else if(!strcmp(argv[1],"nublado")){
+            cout << "Vou ao cinema.\n\n";
+        }else{
+            cout << "Vou ficar encasa.";
+        }
+    }
+    system("pause");
     return 0;
 }
