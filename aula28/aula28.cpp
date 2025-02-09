dcb9ea60632aca42b42c06f091fc64edcf795966
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-09 17:13:32 +0200

    aula 28 aprende a trbalhabar com os methodos da pilha/stack  size, push, pop, top.

--- aula28/aula28.cpp
+++ aula28/aula28.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-using namespace std;
-int main(){
+#include <stack>
 
+using namespace std;
+int main()
+{
+    stack<string> cartas;
+
+    cartas.push("Rei de Copas");
+    cartas.push("Rei de Espadas");
+    cartas.push("Rei de Ouros");
+    cartas.push("Rei de Paus");
+
+    cout << "Tamanho da pilha: " << cartas.size() << "\n";
+    cout << "Carta do topo: " << cartas.top() << "\n\n";
+    cartas.pop();
+    cartas.pop();
+
+    cout << "Update do Tamanho da pilha: " << cartas.size() << "\n";
+    cout << "Nova carta do topo: " << cartas.top() << "\n";
     return 0;
 }
