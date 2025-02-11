776b97cbc87a39ebafa2e2a0fc6ba53844c58f94
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-11 22:34:21 +0200

    Aula 33 feita com sucesso, aprendi a trabalhar com struct e criei um obejecto que variaveis do tipo string e int

--- aula33/aula33.cpp
+++ aula33/aula33.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 using namespace std;
+struct Carro
+{
+    string nome;
+    string cor;
+    int pot;
+    int velMax;
+};
+int main()
+{
+    Carro car1;
+    Carro car2;
 
-int main(){
+    car1.nome = "Tornado";
+    car1.cor = "Vermelho";
+    car1.pot = 450;
+    car1.velMax = 350;
 
-    return 0;
+    car2.nome = "Luxo";
+    car2.cor = "Preto";
+    car2.pot = 250;
+    car2.velMax = 260;
+
+    cout << "Nome....: " << car1.nome << "\n";
+    cout << "Cor.....: " << car1.cor << "\n";
+    cout << "Potencia: " << car1.pot << "\n";
+    cout << "velmax..: " << car1.velMax << "\n";
+
+    cout << "Nome....: " << car2.nome << "\n";
+    cout << "Cor.....: " << car2.cor << "\n";
+    cout << "Potencia: " << car2.pot << "\n";
+    cout << "velmax..: " << car2.velMax << "\n";
 }
