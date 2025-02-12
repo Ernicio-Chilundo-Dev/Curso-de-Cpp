e8ba3c3e2a3064b7ca4e56f9df4b0eaecc5bdc97
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-12 09:04:18 +0200

    aula 34 feita com sucesso parte 2 de struct feita com sucesso

--- aula34/aula34.cpp
+++ aula34/aula34.cpp
@@ -1,36 +1,54 @@
-#include  <iostream>
+#include <iostream>
 using namespace std;
-struct Carro{
+struct Carro
+{
     string nome;
     string cor;
     int pot;
     int velMax;
+    int vel;
+
+    void insere(string stnome, string stcor, int stpot, int stvelMax)
+    {
+        nome = stnome;
+        cor = stcor;
+        pot = stpot;
+        velMax = stvelMax;
+        vel = 0;
+    }
+    void mostra()
+    {
+        cout << "\nNome.............: " << nome << "\n";
+        cout << "Cor..............: " << cor << "\n";
+        cout << "Potencia.........: " << pot << "\n";
+        cout << "Velocidade actal.: " << vel << "\n";
+        cout << "Velocidade Maxima: " << velMax << "\n\n";
+    }
+
+    void mudaVel(int mv)
+    {
+        vel = mv;
+        if (vel > velMax)
+        {
+            vel = velMax;
+        }
+        if (vel < 0)
+        {
+            vel = 0;
+        }
+    }
 };
 
 int main()
+
 {
-    Carro car1;
-    Carro car2;
-
-    car1.nome = "Toyota";
-    car1.cor = "Branca";
-    car1.pot = 250;
-    car1.velMax = 180;
-
-    car2.nome = "Daf";
-    car2.cor = "Branca";
-    car2.pot = 250;
-    car2.velMax = 180;
-
-    cout << "Nome....: "<<car1.nome<<"\n";
-    cout << "Cor.....: "<<car1.nome<<"\n";
-    cout << "Potencia: "<<car1.nome<<"\n";
-    cout << "VelMax..: "<<car1.nome<<"\n";
-
-    cout << "Nome....: "<<car2.nome<<"\n";
-    cout << "Cor.....: "<<car2.nome<<"\n";
-    cout << "Potencia: "<<car2.nome<<"\n";
-    cout << "VelMax..: "<<car2.nome<<"\n";
-
+    Carro car1,car2;
+    car1.insere("Toyota", "Branca", 450, 350);
+    // car2.insere("Luxo", "Vermelho", 350, 250);
+    
+    car1.mostra();
+    car1.mudaVel(500);
+    car1.mostra();
+    
     return 0;
 }
