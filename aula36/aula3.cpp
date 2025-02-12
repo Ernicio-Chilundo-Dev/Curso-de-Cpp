9d8fc34a03d499a9ec0dcbcb75db0cc29b0c61d8
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-12 15:52:42 +0200

    aula 36 feita com sucesso aprendendo a trabalhar com ponteiro

--- aula36/aula3.cpp
+++ aula36/aula3.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main(){
+    string veiculo = "Carro";
+    string *pv;
 
+    pv = &veiculo;// Ponteiro PV recebi o enderenco da variavel Veiculo.
+
+    cout << pv << "\n"<< &veiculo << "\n";
+
+    *pv = "Moto";// No enderenco apontado pelo *PV adicione o valor "Moto"
+
+    cout << *pv<< "\n"<<veiculo;
     return 0;
 }
