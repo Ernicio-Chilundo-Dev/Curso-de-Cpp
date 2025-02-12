8c7eaffb920609c67fcf62b80975c3e250d3686f
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-12 17:13:01 +0200

    aula 38 feita com sucesso aprende a asssociar vetor com ponteiro

--- aula38/aula38.cpp
+++ aula38/aula38.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
+void soma(float *var, float valor);
+void ptar(float *v);
 
-int main(){
+int main()
+{
+    float vetor[5];
+    float num = 0;
+    soma(&num,15);
+    ptar(vetor);
+    cout << num << "\n";
 
+    for( int i = 0; i < 5; i++){
+        cout << vetor[i] << "\n";
+    }
     return 0;
+}
+
+void soma(float *var, float valor){
+    *var+=valor;
+}
+
+void ptar(float *v){
+    v[0]=9;
+    v[1]=9;
+    v[2]=9;
+    v[3]=9;
+    v[4]=9;
 }
