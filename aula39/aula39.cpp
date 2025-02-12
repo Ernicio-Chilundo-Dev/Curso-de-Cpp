21dc6bc94cccc73803240503be77d99e7796d43a
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-13 01:19:09 +0200

    aula 39 concluida, aprende a fazer alocacao dinamica da memoria

--- aula39/aula39.cpp
+++ aula39/aula39.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <stdio.h>// para funcao gets
+#include <stdlib.h> //para funcao malloc
 using namespace std;
 
 int main(){
 
+    char *vnome;
+    vnome = (char *) malloc(sizeof(char)+1);
+
+    gets(vnome);
+    cout << vnome;
     return 0;
 }
