89375fc24c73262c111b526493e2012423cc21ad
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-02-14 10:23:32 +0200

    aula43 feita com sucesso aprendi a trabalhar com o method system

---
+++ aula43/aula43.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    system("color 46");
+    // system("dir");
+    // system("cls");
+
+    cout << "Ernicio Jermias Chilundo";
+
+    return 0;
+
+}
